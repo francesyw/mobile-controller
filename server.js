7e72246f971ccbdd391ca9b3410be5650a459825
Frances Wang <francesyuanwang@gmail.com>
2018-05-22 16:11:49 -0400

    using webpack

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const server = app.listen(1337, function () {
-  console.log('Server running...');
+  console.log('Server running on 1337...');
 })
+
+app.use(express.static(path.join(__dirname, '.', 'public')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '.', 'public/index.html'));
+});
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './public/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+}
