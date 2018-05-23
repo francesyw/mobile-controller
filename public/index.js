f9a20c387152208f649bc018d97a638f65d4dce1
Frances Wang <francesyuanwang@gmail.com>
2018-05-22 23:24:53 -0400

    added p5.js

--- public/index.js
+++ public/index.js
@@ -3,3 +3,12 @@ const socket = io.connect(window.location.origin);
 socket.on('connect', () => {
   console.log('browser connected to server');
 });
+
+function setup() {
+  let cnv = createCanvas(500, 500);
+  cnv.position((windowWidth - width)/2, 130);
+}
+
+function draw() {
+  background(220);
+}
